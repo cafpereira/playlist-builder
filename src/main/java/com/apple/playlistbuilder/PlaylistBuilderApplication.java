7dbaae8d92e633669bba3218d037ab47206d5201
cpereira <cpereira@groupon.com>
2017-07-09 20:19:51 -0400

    initial commit

---
+++ src/main/java/com/apple/playlistbuilder/PlaylistBuilderApplication.java
@@ -0,0 +1,12 @@
+package com.apple.playlistbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistBuilderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/apple/playlistbuilder/PlaylistBuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apple.playlistbuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaylistBuilderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
